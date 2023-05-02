e7ace25de9424d94c9daf89e18837d001fc3e1f6
REFATBHUYAN <refatbubt@gmail.com>
2023-05-02 22:53:02 +0600

    data successfull showd in server

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefData = require('./data/chefData.json');
+
 app.get('/',(req,res)=>{
     res.send('server running .................')
+})
+
+app.get('/chef',(req,res)=>{
+    res.send(chefData);
 })
 
 app.listen(port, ()=>{
